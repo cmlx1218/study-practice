f7d54eb4ad212d7d82638ed5a063ca61ce12013a
zheng <zq861500116>
2021-05-11 06:43:51 +0800

    .init:初始化study-practice工程

---
+++ tuling-practice/thread-practice/src/main/java/com/cmlx/thread/ThreadPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.cmlx.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadPracticeApplication.class, args);
+        System.out.println("Tuling-Thread-Practice Start Success!!!");
+    }
+
+}
---
+++ tuling-practice/thread-practice/src/test/java/com/cmlx/thread/ThreadPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmlx.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
