53f004abf22b2fedc353204cf1eba5f14861e5dc
cmlx1218 <cmlx1218@163.com>
2021-05-18 20:41:11 +0800

    .feat:新增多线程日志并行测试类

--- tuling-practice/thread-practice/src/main/java/com/cmlx/thread/parallel/service/impl/ParallelServiceImpl.java
+++ tuling-practice/thread-practice/src/main/java/com/cmlx/thread/parallel/service/impl/ParallelServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cmlx.thread.parallel.service.impl;
 
+import com.cmlx.thread.parallel.service.IMainMethodService;
 import com.cmlx.thread.parallel.service.IParallelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Method;
@@ -14,54 +16,93 @@ import java.util.concurrent.*;
 @Service
 public class ParallelServiceImpl implements IParallelService {
 
+    @Autowired
+    private IMainMethodService iMainMethodService;
+
     @Override
     public void testParallel() throws ExecutionException, InterruptedException {
         // 三个线程的线程池，核心线程=最大线程，没有临时线程，阻塞队列无界
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
         long startTime = System.currentTimeMillis();
         // 开启线程执行
         // 注意，此处Future对象接收线程执行结果不会阻塞，只有future.get()时候才会阻塞（直到线程执行完返回结果）
-        Future future1 = executorService.submit(new SelectTask<>(this, "add"));
+        Future future1 = executorService.submit(new add());
 
-        Future future2 = executorService.submit(new SelectTask<>(this, "sub"));
+        Future future2 = executorService.submit(new sub());
 
-        Future future3 = executorService.submit(new SelectTask<>(this, "mul"));
+        Future future3 = executorService.submit(new mul());
 
-        Future future4 = executorService.submit(new SelectTask<>(this, "div"));
+        Future future4 = executorService.submit(new div());
 
         //此处用循环保证三个线程执行完毕，再去拼接三个结果
         do {
-            System.out.println("多任务同时执行中...");
+            //System.out.println("多任务同时执行中...");
         } while (!(future1.isDone() && future2.isDone() && future3.isDone() && future4.isDone()));
 
         long end = System.currentTimeMillis();
         System.out.println("并行执行：" + (end - startTime));
-
+        executorService.shutdown();
     }
 
-    //任务线程类
-    class SelectTask<T> implements Callable<T> {
-
-        private Object object;
-        private Object[] args;
-        private String methodName;
-
-        public SelectTask(Object object, String methodName, Object[] args) {
-            this.object = object;
-            this.args = args;
-            this.methodName = methodName;
-        }
-
-        public SelectTask(Object object, String methodName) {
-            this.object = object;
-            this.methodName = methodName;
-        }
-
+    class add implements Callable {
 
         @Override
-        public T call() throws Exception {
-            Method method = object.getClass().getMethod(methodName, String.class);   //此处应用反射机制，String.class是根据实际方法参数设置的
-            return (T) method.invoke(object);
+        public Object call() throws Exception {
+            iMainMethodService.add();
+            return "add";
         }
     }
+
+    class sub implements Callable {
+
+        @Override
+        public Object call() throws Exception {
+            iMainMethodService.sub();
+            return "sub";
+        }
+    }
+
+    class mul implements Callable {
+
+        @Override
+        public Object call() throws Exception {
+            iMainMethodService.mul();
+            return "mul";
+        }
+    }
+
+    class div implements Callable {
+
+        @Override
+        public Object call() throws Exception {
+            iMainMethodService.div();
+            return "div";
+        }
+    }
+
+    ////任务线程类
+    //class SelectTask<T> implements Callable<T> {
+    //
+    //    private Object object;
+    //    private Object[] args;
+    //    private String methodName;
+    //
+    //    public SelectTask(Object object, String methodName, Object[] args) {
+    //        this.object = object;
+    //        this.args = args;
+    //        this.methodName = methodName;
+    //    }
+    //
+    //    public SelectTask(Object object, String methodName) {
+    //        this.object = object;
+    //        this.methodName = methodName;
+    //    }
+    //
+    //
+    //    @Override
+    //    public T call() throws Exception {
+    //        Method method = object.getClass().getMethod(methodName, String.class);   //此处应用反射机制，String.class是根据实际方法参数设置的
+    //        return (T) method.invoke(object);
+    //    }
+    //}
 }
