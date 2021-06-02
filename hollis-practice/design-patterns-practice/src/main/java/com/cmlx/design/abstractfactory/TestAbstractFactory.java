3088b08985e9d7b08bdf55675767b3947ed172fe
cmlx1218 <cmlx1218@163.com>
2021-06-02 16:56:22 +0800

    .feat:测试nacos密码加密

--- hollis-practice/design-patterns-practice/src/main/java/com/cmlx/design/abstractfactory/TestAbstractFactory.java
+++ hollis-practice/design-patterns-practice/src/main/java/com/cmlx/design/abstractfactory/TestAbstractFactory.java
@@ -2,6 +2,7 @@ package com.cmlx.design.abstractfactory;
 
 import com.cmlx.design.abstractfactory.factory.impl.SportCarFactory;
 import com.cmlx.design.abstractfactory.product.BenzCar;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @Author CMLX
@@ -11,10 +12,13 @@ import com.cmlx.design.abstractfactory.product.BenzCar;
 public class TestAbstractFactory {
 
     public static void main(String[] args) {
-        SportCarFactory sportCarFactory = new SportCarFactory();
-        BenzCar benzCar = sportCarFactory.getBenzCar();
-        benzCar.gasUp();
-    }
+        //SportCarFactory sportCarFactory = new SportCarFactory();
+        //BenzCar benzCar = sportCarFactory.getBenzCar();
+        //benzCar.gasUp();
 
+        String nacos = new BCryptPasswordEncoder().encode("aimy2021#$&!");
+        System.out.println(nacos);
+
+    }
 
 }
