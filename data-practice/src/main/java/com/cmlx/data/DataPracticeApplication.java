4b35107d6c3643251d39bed163a2ea1164658135
zheng <zq861500116>
2021-08-10 21:49:22 +0800

    .feat:新增稀疏数组相关

---
+++ data-practice/src/main/java/com/cmlx/data/DataPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cmlx.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPracticeApplication.class, args);
+    }
+
+}
